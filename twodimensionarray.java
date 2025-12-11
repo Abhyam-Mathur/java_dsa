5d4e1ceed9c019eeedf52ef4a1f1c70958ddeff2
Abhyam-Mathur <154632064+Abhyam-Mathur@users.noreply.github.com>
2025-12-11 22:57:39 +0530

    2d array

--- twodimensionarray.java
+++ twodimensionarray.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class twodimensionarray{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the two dimension of your 2d array");
+        System.out.println("Enter the rows and columns dimension of your 2d array");
         int row= in.nextInt();
         int column = in.nextInt();
         int arr[][]=new int[row][column];
-        System.out.println("Enter the elements of your 2d array");
+        System.out.print("Enter the elements of your 2d array: ");
         for(int i = 0;i<row;i++){
             for(int j=0;j<column;j++){
                 arr[i][j]= in.nextInt();
@@ -14,7 +14,7 @@ public class twodimensionarray{
         }
         for(int i=0;i<row;i++){
             for(int j=0;j<column;j++){
-                System.out.print(arr[i][j]);
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
