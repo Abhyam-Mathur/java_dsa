e5657a0c898cdeb6da47267e33e667da24209e57
Abhyam Mathur <154632064+Abhyam-Mathur@users.noreply.github.com>
2025-12-29 14:24:03 +0530

    reverse a no

--- reverse.java
+++ reverse.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class pailndrome {
+public class reverse {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a no ");
