66be8f86ec0cde45a71c90cc0e7d060303c0bc2b
Abhyam-Mathur <154632064+Abhyam-Mathur@users.noreply.github.com>
2025-12-31 00:56:33 +0530

    using function

--- calcfun.java
+++ calcfun.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 public class calcfun {
     public static void main(String[] args) {
-    sum();
-    }
-    static void sum(){
         Scanner in = new Scanner(System.in);
         System.out.println("enter a no");
         int a= in.nextInt();
         int b= in.nextInt();
+        int result = sum(a,b);
+        System.out.println("sum of two no is: "+ result );
+    }
+    static int sum(int a, int b){
         int result= a+b;
-        System.out.println("sum is: "+result);
+        return result;
     }
 }
