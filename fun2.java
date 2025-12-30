6e9530ba4bb208bbcf023e969531f7aaa65d12cd
Abhyam-Mathur <154632064+Abhyam-Mathur@users.noreply.github.com>
2025-12-31 01:21:45 +0530

    using function

---
+++ fun2.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class fun2 {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
+        swap(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] a){
+        a[0]=0;
+    }
+}
