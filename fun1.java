cb7bda20cafb53403bbb169ed5ad765552b2dd3a
Abhyam-Mathur <154632064+Abhyam-Mathur@users.noreply.github.com>
2025-12-31 01:13:49 +0530

    using function

---
+++ fun1.java
@@ -0,0 +1,10 @@
+public class fun1 {
+    public static void main(String[] args) {
+        String name="abhyam";
+        swap(name);     // swapping won't be done
+        System.out.println(name);
+    }
+    static void swap(String naam){
+        naam="shivam"; // creating a new object
+    }
+}
