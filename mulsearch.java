8e0e8202253caef36f2ae91b4de692353444b9c8
Abhyam-Mathur <154632064+Abhyam-Mathur@users.noreply.github.com>
2026-01-04 21:52:33 +0530

    search in 2d array

---
+++ mulsearch.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class mulsearch {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter the diamentions of the array: ");
+        int row= in.nextInt();
+        int col= in.nextInt();
+        int [][] arr= new int[row][col];
+        System.out.println("enter the no in array: ");
+        for (int i = 0; i <row ; i++) {
+            for (int j = 0; j <col ; j++) {
+                arr[i][j]= in.nextInt();
+            }
+        }
+      //  for(int[] a : arr) {
+      //      System.out.println(Arrays.toString((a)));
+      //  }
+        System.out.println("Enter the element to search ");
+        int x= in.nextInt();
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j <arr[i].length ; j++) {
+                if(arr[i][j]==x){
+                    System.out.println("yes present in array: "+arr[i][j]);
+                }
+            }
+
+        }
+    }
+}
